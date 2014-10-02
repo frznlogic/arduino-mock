a6b82370f0323b34049e14305a0052e1c37fa4f2
ikeyasu <ikeyasu@gmail.com>
2014-10-03 08:07:11 +0900

    fix

--- script/formatting_checker.sh
+++ script/formatting_checker.sh
@@ -5,7 +5,9 @@ cd "$(dirname -- "$0")"
 LANG=C 
 astyle -X --style=google -n ../**/*.cc ../include/**/*.h | grep Formatted
 if [ $? = 0 ]; then
+	echo ""
 	echo "Some files need to format."
+	echo "Please run 'make format' in test/ directory."
 	exit 1
 else
 	exit 0
