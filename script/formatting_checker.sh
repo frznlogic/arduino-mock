e28e7e9b45aabda60690b250702a471efa45b400
ikeyasu <ikeyasu@gmail.com>
2014-10-03 08:03:02 +0900

    fix

--- script/formatting_checker.sh
+++ script/formatting_checker.sh
@@ -4,10 +4,10 @@ cd "$(dirname -- "$0")"
 
 LANG=C 
 astyle -X --style=google -n ../**/*.cc ../include/**/*.h | grep Formatted
-if $?; then
+if [ $? = 0 ]; then
 	echo "Some files need to format."
 	exit 1
 else
 	exit 0
-end
+fi
 
